ac83fe1ee5d0844fd6397700b15bf1901bfdcfc8
Ihor <108004180+beligrous@users.noreply.github.com>
2023-07-31 16:18:04 +0300

    add main and header template

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./header-styled";
+
+const Header = () => {
+  return (
+    <Container>
+      <p>header</p>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/header-styled.js
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const Container = styled.header`
+  border: 2px solid black;
+  width: 100%;
+`;
